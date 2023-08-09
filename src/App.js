915b02e88dadd026c36d7eaf6d5c27057917ee24
Marko Gabric <mgabri00@fesb.hr>
2023-08-09 08:29:32 +0200

    Finished Navbar, added Hero draft, changed primary color #5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import './App.css';
-import tw from 'twin.macro';
-
-export const NavLinks = tw.div`inline-block`;
-
-const NavLink = tw.a`
-text-lg my-2 lg:text-sm lg:mx-6 lg:my-0
-font-semibold tracking-wide transition duration-300
-pb-1 border-b-2 border-transparent hover:border-primary-500 hocus:text-primary-500
-`
+import Navbar from './components/navbar';
+import Hero from './components/hero';
+import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
   return (
-    <div className="text-center font-display">
-      <NavLinks>
-        <NavLink>Search</NavLink>
-        <NavLink>Blog</NavLink>
-        <NavLink>About</NavLink>
-        <NavLink>Contact Us</NavLink>
-      </NavLinks>
-    </div>
+    <>
+      <GlobalStyles />
+      <div className="text-center font-display p-8">
+        <Navbar />
+        <Hero />
+      </div>
+    </>
   );
 }
 
-export default App;
+export default App; 
